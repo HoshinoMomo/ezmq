d0c4e4cd83ffdb909ed1a090652aa2f4dc4bbe96
zhangyi02 <zhangyi02@daojia-inc.com>
2018-09-11 15:17:06 +0800

    freedom

---
+++ src/main/java/com/github/softeasyzhang/entity/Message.java
@@ -0,0 +1,10 @@
+package com.github.softeasyzhang.entity;
+
+
+/**
+ * you car write what you want.
+ * @author EasyZhang
+ * @date 2018/9/11 -  15:03
+ */
+
+public class Message {}
---
+++ src/main/java/com/github/softeasyzhang/entity/SearchRequest.java
@@ -0,0 +1,11 @@
+package com.github.softeasyzhang.entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author EasyZhang
+ * @date 2018/9/11 -  15:12
+ */
+
+public class SearchRequest {}
