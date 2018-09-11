4fc75c42c3a04acc90d884bb03d752c34ced61f8
zhangyi02 <zhangyi02@daojia-inc.com>
2018-09-11 14:41:22 +0800

    snapshot

--- src/main/java/com/github/softeasyzhang/init/EzmqInit.java
+++ src/main/java/com/github/softeasyzhang/init/EzmqInit.java
@@ -1,11 +1,16 @@
 package com.github.softeasyzhang.init;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author EasyZhang
- * @date 2018/9/11 -  10:36
+ * @date 2018/9/11 -  14:39
  */
 
 public class EzmqInit {
+
+    private static final Logger logger = LoggerFactory.getLogger(EzmqInit.class);
+
 
 }
